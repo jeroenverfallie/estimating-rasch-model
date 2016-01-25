b62930ff9b697fde4b8a23e94722e877002d173e
SanVerhavert <san.verhavert@uantwerpen.be>
2016-01-25 16:56:17 +0100

    Mocha testing adapted (and successful)

--- tests/estimation.spec.js
+++ tests/estimation.spec.js
@@ -1,15 +1,91 @@
 'use strict';
 
-var expect = require( 'must' );
-var subject = require( '../lib/estimation' );
-var fixtures = require( './fixtures' );
+var expect = require('must');
+var subject = require('../lib/estimation');
+var _ = require('lodash');
+var fixtures = require('./fixtures');
 
-describe( 'estimating rasch model', function(){
-  describe( 'the estimation results', function(){
-    it( 'should equal R generated results', function(){
-      expect( fixtures.representations ).to.not.eql( fixtures.results );
-      subject.estimateCJ( fixtures.comparisons, fixtures.representations );
-      expect( fixtures.representations ).to.eql( fixtures.results );
-    } );
-  } );
-} );
+
+describe('estimating rasch model', function () {
+    describe("spec file", function () {
+        it("must be found", function () {
+            expect(true).to.be.true();
+        });
+    });
+
+    describe('the estimation results', function () {
+        describe('if not all representations are to rank', function () {
+
+            var ResultsValue = _.map(fixtures.resultsPartRanked, function (resultRepr) {
+                return _.get(resultRepr.ability, "value")
+            });
+
+
+            var ResultsSe = _.map(fixtures.resultsPartRanked, function (resultRepr) {
+                return _.get(resultRepr.ability, "se")
+            });
+
+            var estRepresentations = fixtures.representationsPartRanked;
+
+            beforeEach( function(){
+                _.forEach( estRepresentations, function( repr ){
+                    if( _.get( repr, "type" ) == "to rank")
+                        repr.ability = {
+                            "value": 0,
+                            "se": 0
+                        }
+                })
+            });
+
+            it('should equal R generated results if all data are provided at once', function () {
+                expect(estRepresentations).to.not.eql(fixtures.resultsPartRanked);
+
+                subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
+
+
+                var groups = _.groupBy(estRepresentations, '_id');
+
+                var estimatesValue = _.map(fixtures.resultsPartRanked, function (i) {
+                    var temp = _.get( groups, i._id );
+                    return  _.round( _.get(temp, [ "0", "ability", "value" ] ), 4 );
+                });
+
+                var estimatesSe = _.map(fixtures.resultsPartRanked, function (i) {
+                    var temp = _.get( groups, i._id );
+                    return  _.round( _.get(temp, [ "0", "ability", "se" ] ), 4 );
+                });
+
+                expect(estimatesValue).to.eql(ResultsValue);
+                expect(estimatesSe).to.eql(ResultsSe);
+            });
+
+            it('should equal R generated results if estimates were first based on a part of the data', function () {
+                expect(estRepresentations).to.not.eql(fixtures.resultsPartRanked);
+
+                var Ncomparisons = fixtures.comparisonsPartRanked.length;
+                var NcomparisonsSplit = Ncomparisons / 2;
+
+                var comparisonsA = fixtures.comparisonsPartRanked.slice( 0,NcomparisonsSplit );
+
+                subject.estimateCJ(comparisonsA, estRepresentations);
+                subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
+
+                var groups = _.groupBy(estRepresentations, '_id');
+
+                var estimatesValue = _.map(fixtures.resultsPartRanked, function (i) {
+                    var temp = _.get( groups, i._id );
+                    return  _.round( _.get(temp, [ "0", "ability", "value" ] ), 4 );
+                });
+
+                var estimatesSe = _.map(fixtures.resultsPartRanked, function (i) {
+                    var temp = _.get( groups, i._id );
+                    return  _.round( _.get(temp, [ "0", "ability", "se" ] ), 4 );
+                });
+
+                expect(estimatesValue).to.eql(ResultsValue);
+                expect(estimatesSe).to.eql(ResultsSe);
+            });
+        });
+
+    });
+});
