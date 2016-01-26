e4612d86ab6ea51777ac014d6b18eaeb8ba98d15
Camille Reynders <camille.reynders@gmail.com>
2016-01-26 12:42:11 +0100

    Reset representation.selectedNum just to be sure

--- tests/estimation.spec.js
+++ tests/estimation.spec.js
@@ -16,43 +16,24 @@ describe('estimating rasch model', function () {
     describe('the estimation results', function () {
         describe('if not all representations are to rank', function () {
 
-            var ResultsValue = _.map(fixtures.resultsPartRanked, function (resultRepr) {
-                return _.get(resultRepr.ability, "value")
-            });
-
-
-            var ResultsSe = _.map(fixtures.resultsPartRanked, function (resultRepr) {
-                return _.get(resultRepr.ability, "se")
-            });
-
-            var estRepresentations = fixtures.representationsPartRanked;
-
+            var ResultsValue = _.map(fixtures.resultsPartRanked, "ability.value");
+            var ResultsSe = _.map(fixtures.resultsPartRanked, "ability.se");
+            var estRepresentations;
+            
             beforeEach( function(){
-                _.forEach( estRepresentations, function( repr ){
-                    if( _.get( repr, "type" ) == "to rank")
-                        repr.ability = {
-                            "value": 0,
-                            "se": 0
-                        }
-                })
+                estRepresentations = _.cloneDeep(fixtures.representationsPartRanked);
             });
 
             it('should equal R generated results if all data are provided at once', function () {
                 expect(estRepresentations).to.not.eql(fixtures.resultsPartRanked);
 
                 subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
-
-
-                var groups = _.groupBy(estRepresentations, '_id');
-
-                var estimatesValue = _.map(fixtures.resultsPartRanked, function (i) {
-                    var temp = _.get( groups, i._id );
-                    return  _.round( _.get(temp, [ "0", "ability", "value" ] ), 4 );
-                });
-
-                var estimatesSe = _.map(fixtures.resultsPartRanked, function (i) {
-                    var temp = _.get( groups, i._id );
-                    return  _.round( _.get(temp, [ "0", "ability", "se" ] ), 4 );
+                
+                var estimatesValue = [];
+                var estimatesSe = [];
+                _.each(estRepresentations, function(representation){
+                    estimatesValue.push(_.round(representation.ability.value, 4));
+                    estimatesSe.push(_.round(representation.ability.se, 4));
                 });
 
                 expect(estimatesValue).to.eql(ResultsValue);
@@ -70,16 +51,11 @@ describe('estimating rasch model', function () {
                 subject.estimateCJ(comparisonsA, estRepresentations);
                 subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
 
-                var groups = _.groupBy(estRepresentations, '_id');
-
-                var estimatesValue = _.map(fixtures.resultsPartRanked, function (i) {
-                    var temp = _.get( groups, i._id );
-                    return  _.round( _.get(temp, [ "0", "ability", "value" ] ), 4 );
-                });
-
-                var estimatesSe = _.map(fixtures.resultsPartRanked, function (i) {
-                    var temp = _.get( groups, i._id );
-                    return  _.round( _.get(temp, [ "0", "ability", "se" ] ), 4 );
+                var estimatesValue = [];
+                var estimatesSe = [];
+                _.each(estRepresentations, function(representation){
+                    estimatesValue.push(_.round(representation.ability.value, 4));
+                    estimatesSe.push(_.round(representation.ability.se, 4));
                 });
 
                 expect(estimatesValue).to.eql(ResultsValue);
