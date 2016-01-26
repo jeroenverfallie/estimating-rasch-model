2893f3ea7d1534ffd3fb980561f0d27cf9513a8d
SanVerhavert <san.verhavert@uantwerpen.be>
2016-01-26 13:49:12 +0100

    unit tests added and successful

--- tests/estimation.spec.js
+++ tests/estimation.spec.js
@@ -19,8 +19,8 @@ describe('estimating rasch model', function () {
             var ResultsValue = _.map(fixtures.resultsPartRanked, "ability.value");
             var ResultsSe = _.map(fixtures.resultsPartRanked, "ability.se");
             var estRepresentations;
-            
-            beforeEach( function(){
+
+            beforeEach(function () {
                 estRepresentations = _.cloneDeep(fixtures.repsPartRanked);
             });
 
@@ -28,10 +28,10 @@ describe('estimating rasch model', function () {
                 expect(estRepresentations).to.not.eql(fixtures.resultsPartRanked);
 
                 subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
-                
+
                 var estimatesValue = [];
                 var estimatesSe = [];
-                _.each(estRepresentations, function(representation){
+                _.each(estRepresentations, function (representation) {
                     estimatesValue.push(_.round(representation.ability.value, 4));
                     estimatesSe.push(_.round(representation.ability.se, 4));
                 });
@@ -46,14 +46,14 @@ describe('estimating rasch model', function () {
                 var Ncomparisons = fixtures.comparisonsPartRanked.length;
                 var NcomparisonsSplit = Ncomparisons / 2;
 
-                var comparisonsA = fixtures.comparisonsPartRanked.slice( 0,NcomparisonsSplit );
+                var comparisonsA = fixtures.comparisonsPartRanked.slice(0, NcomparisonsSplit);
 
                 subject.estimateCJ(comparisonsA, estRepresentations);
                 subject.estimateCJ(fixtures.comparisonsPartRanked, estRepresentations);
 
                 var estimatesValue = [];
                 var estimatesSe = [];
-                _.each(estRepresentations, function(representation){
+                _.each(estRepresentations, function (representation) {
                     estimatesValue.push(_.round(representation.ability.value, 4));
                     estimatesSe.push(_.round(representation.ability.se, 4));
                 });
@@ -63,5 +63,53 @@ describe('estimating rasch model', function () {
             });
         });
 
+        describe('if all representations are to rank', function () {
+
+            var ResultsValue = _.map(fixtures.resultsNoneRanked, "ability.value");
+            var ResultsSe = _.map(fixtures.resultsNoneRanked, "ability.se");
+            var estRepresentations;
+
+            beforeEach(function () {
+                estRepresentations = _.cloneDeep(fixtures.repsNoneRanked);
+            });
+
+            it('should equal R generated results if all data are provided at once', function () {
+                expect(estRepresentations).to.not.eql(fixtures.resultsNoneRanked);
+
+                subject.estimateCJ(fixtures.comparisonsNoneRanked, estRepresentations);
+
+                var estimatesValue = [];
+                var estimatesSe = [];
+                _.each(estRepresentations, function (representation) {
+                    estimatesValue.push(_.round(representation.ability.value, 4));
+                    estimatesSe.push(_.round(representation.ability.se, 4));
+                });
+
+                expect(estimatesValue).to.eql(ResultsValue);
+                expect(estimatesSe).to.eql(ResultsSe);
+            });
+
+            it('should equal R generated results if estimates were first based on a part of the data', function () {
+                expect(estRepresentations).to.not.eql(fixtures.resultsNoneRanked);
+
+                var Ncomparisons = fixtures.comparisonsNoneRanked.length;
+                var NcomparisonsSplit = Ncomparisons / 2;
+
+                var comparisonsA = fixtures.comparisonsNoneRanked.slice(0, NcomparisonsSplit);
+
+                subject.estimateCJ(comparisonsA, estRepresentations);
+                subject.estimateCJ(fixtures.comparisonsNoneRanked, estRepresentations);
+
+                var estimatesValue = [];
+                var estimatesSe = [];
+                _.each(estRepresentations, function (representation) {
+                    estimatesValue.push(_.round(representation.ability.value, 4));
+                    estimatesSe.push(_.round(representation.ability.se, 4));
+                });
+
+                expect(estimatesValue).to.eql(ResultsValue);
+                expect(estimatesSe).to.eql(ResultsSe);
+            });
+        });
     });
 });
